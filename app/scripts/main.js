f317600b86f4a83285bd767be16b02ae017f73b3
Joe <josephdburdick@gmail.com>
2015-09-21 15:37:40 -0400

    Add page mock, custom column size for 16:9 media, moving video upon mousehover, click to fullscreen.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  var
+    $video = $('#demo-video'),
+    $body = $('body');
+    $video
+      .on('mouseenter', function(){
+        $video.get(0).play();
+      })
+      .on('mouseleave', function(){
+        $video.get(0).pause();
+        $video.get(0).currentTime = 0;
+      })
+      .on('click', function(e){
+        e.preventDefault();
+        var fullScreenVideoURL = this.dataset.fullscreenvideo;
+
+        if (!$body.find('#video-fullscreen').length)
+          $body.append('<div id="video-fullscreen"><video><source></source></video></div>');
+        var $fullScreenVideoContainer = $('#video-fullscreen'),
+            $fullScreenVideo = $fullScreenVideoContainer.find('video');
+
+        $fullScreenVideo
+          .find('source').attr({
+            src: fullScreenVideoURL,
+            type: "video/mp4"
+          });
+        $fullScreenVideo.get(0).play();
+
+        if ($fullScreenVideo.get(0).requestFullscreen) {
+          $fullScreenVideo.get(0).requestFullscreen();
+        } else if ($fullScreenVideo.get(0).mozRequestFullScreen) {
+          $fullScreenVideo.get(0).mozRequestFullScreen();
+        } else if ($fullScreenVideo.get(0).webkitRequestFullscreen) {
+          $fullScreenVideo.get(0).webkitRequestFullscreen();
+        }
+      });
+
+});
