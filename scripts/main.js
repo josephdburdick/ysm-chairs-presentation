28081cb9a21ecfd1615091142a636c24184a2b95
Joe <josephdburdick@gmail.com>
2015-09-21 19:16:51 -0400

    Update 2015-09-21T23:16:39.137Z

--- scripts/main.js
+++ scripts/main.js
@@ -32,44 +32,61 @@ $(function () {
     } else if ($fullScreenVideo.get(0).webkitRequestFullscreen) {
       $fullScreenVideo.get(0).webkitRequestFullscreen();
     }
+
+    $fullScreenVideo.on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function (e) {
+      var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+      var event = state ? 'FullscreenOn' : 'FullscreenOff';
+      if (event === "FullscreenOff") {
+        $fullScreenVideo.get(0).pause();
+        $fullScreenVideo.get(0).currentTime = 0;
+        $fullScreenVideo = null;
+        $('#video-fullscreen').remove();
+      }
+    });
   });
 });
-//# sourceMappingURL=main.js.map
 /*global $:false*/
-$(() => {
-  let $video = $('#demo-video'),
-    $body = $('body');
-    $video
-      .on('mouseenter', () => {
-        $video.get(0).play();
-      })
-      .on('mouseleave', () => {
-        $video.get(0).pause();
-        $video.get(0).currentTime = 0;
-      })
-      .on('click', (e) => {
-        e.preventDefault();
-        let fullScreenVideoURL = e.currentTarget.dataset.fullscreenvideo;
+$(function () {
+  var $video = $('#demo-video'),
+      $body = $('body');
+  $video.on('mouseenter', function () {
+    $video.get(0).play();
+  }).on('mouseleave', function () {
+    $video.get(0).pause();
+    $video.get(0).currentTime = 0;
+  }).on('click', function (e) {
+    e.preventDefault();
+    var fullScreenVideoURL = e.currentTarget.dataset.fullscreenvideo;
 
-        if (!$body.find('#video-fullscreen').length){
-          $body.append('<div id="video-fullscreen"><video><source></source></video></div>');
-        }
-        let $fullScreenVideoContainer = $('#video-fullscreen'),
-            $fullScreenVideo = $fullScreenVideoContainer.find('video');
+    if (!$body.find('#video-fullscreen').length) {
+      $body.append('<div id="video-fullscreen"><video><source></source></video></div>');
+    }
+    var $fullScreenVideoContainer = $('#video-fullscreen'),
+        $fullScreenVideo = $fullScreenVideoContainer.find('video');
 
-        $fullScreenVideo
-          .find('source').attr({
-            src: fullScreenVideoURL,
-            type: 'video/mp4'
-          });
-        $fullScreenVideo.get(0).play();
+    $fullScreenVideo.find('source').attr({
+      src: fullScreenVideoURL,
+      type: 'video/mp4'
+    });
+    $fullScreenVideo.get(0).play();
 
-        if ($fullScreenVideo.get(0).requestFullscreen) {
-          $fullScreenVideo.get(0).requestFullscreen();
-        } else if ($fullScreenVideo.get(0).mozRequestFullScreen) {
-          $fullScreenVideo.get(0).mozRequestFullScreen();
-        } else if ($fullScreenVideo.get(0).webkitRequestFullscreen) {
-          $fullScreenVideo.get(0).webkitRequestFullscreen();
-        }
-      });
+    if ($fullScreenVideo.get(0).requestFullscreen) {
+      $fullScreenVideo.get(0).requestFullscreen();
+    } else if ($fullScreenVideo.get(0).mozRequestFullScreen) {
+      $fullScreenVideo.get(0).mozRequestFullScreen();
+    } else if ($fullScreenVideo.get(0).webkitRequestFullscreen) {
+      $fullScreenVideo.get(0).webkitRequestFullscreen();
+    }
+
+    $fullScreenVideo.on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function (e) {
+      var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+      var event = state ? 'FullscreenOn' : 'FullscreenOff';
+      if (event === "FullscreenOff") {
+        $fullScreenVideo.get(0).pause();
+        $fullScreenVideo.get(0).currentTime = 0;
+        $fullScreenVideo = null;
+        $('#video-fullscreen').remove();
+      }
+    });
+  });
 });
